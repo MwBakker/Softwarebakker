c5447e41970d484d5cd6a13a57eede2f9bf46947
MwB <martijn.w.bakker@gmail.com>
2024-07-28 16:30:42 +0200

    init commit

---
+++ src/components/block.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="greetings">
+  </div>
+</template>
+
+<script setup>
+defineProps({
+  msg: {
+    type: String,
+    required: true
+  }
+})
+</script>
+
+<style scoped></style>
